8e50acd55ff683b71e40be5a34f34d69cf4d1ada
Mike Moran <mmoran0032@gmail.com>
2017-06-18 13:05:33 -0400

    Fixing some issues with website based on my personal GH one

--- switchyard/views.py
+++ switchyard/views.py
@@ -1,7 +1,7 @@
 
 
 from flask import render_template
-# from flask import request
+from flask import request
 
 # import json
 import os
@@ -14,40 +14,6 @@ from sqlalchemy import create_engine
 from . import app
 
 
-user = 'mikemoran'
-host = 'localhost'
-dbname = 'fullstations'
-db = create_engine(f'postgres://{user}@{host}/{dbname}')
-conn = None
-conn = psycopg2.connect(database=dbname, user=user)
-
-
-@app.route('/')
-@app.route('/index')
-def index():
-    logo_file = get_random_logo()
-    # lines = get_line_characters()
-    details = get_stations_associated_with_lines()
-    details.sort(key=lambda x: x[2])
-    return render_template('index.html',
-                           details=details,
-                           logo_file=logo_file)
-
-
-def get_random_logo():
-    files = list(filter(lambda x: x.endswith('png'),
-                        os.listdir(app.static_folder)))
-    return random.choice(files)
-
-
-def get_line_characters():
-    query = '''
-        select distinct linename from details;
-    '''
-    lines = pd.read_sql(query, conn)['linename'].tolist()
-    return sorted(list(set(''.join(lines))))
-
-
 def get_stations_associated_with_lines():
     query = '''
         select unit, station, linename from details;
@@ -57,4 +23,51 @@ def get_stations_associated_with_lines():
     stations = [details.station[details.unit == u].iloc[-1] for u in units]
     lines = [details.linename[details.unit == u].tolist() for u in units]
     lines = [''.join(sorted(list(set(''.join(l))))) for l in lines]
-    return list(zip(units, stations, lines))
+    details = list(zip(units, stations, lines))
+    details.sort(key=lambda x: x[2])
+    return details
+
+
+user = 'mikemoran'
+host = 'localhost'
+dbname = 'fullstations'
+db = create_engine(f'postgres://{user}@{host}/{dbname}')
+conn = None
+conn = psycopg2.connect(database=dbname, user=user)
+
+details = get_stations_associated_with_lines()
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    unit = request.args.get('unit')
+    index = request.args.get('i')
+    unit = 'R001' if unit is None else unit
+    index = 1 if index is None else index
+    data = pull_data(unit)
+    data_2 = pull_data('R572')
+    logo_file = get_random_logo()
+    return render_template('index.html',
+                           unit=unit,
+                           index=index,
+                           details=details,
+                           logo_file=logo_file,
+                           data=data,
+                           data_2=data_2,
+                           test_data=[1, 2, 3, 4, 5])
+
+
+def pull_data(unit):
+    query = '''
+        select * from {}
+            where date_time > '2016-06-01';
+    '''.format(unit)
+    data = pd.read_sql(query, conn)
+    return data
+
+
+def get_random_logo():
+    files = list(filter(lambda x: x.endswith('png'),
+                        os.listdir(f'{app.static_folder}/images')))
+    return random.choice(files)
