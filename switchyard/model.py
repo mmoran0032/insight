bcc72ca4bc50f0207af0b7af647e1364b2df507b
Mike Moran <mmoran0032@gmail.com>
2017-06-20 21:57:58 -0400

    Working toward fully CSV and model

--- switchyard/model.py
+++ switchyard/model.py
@@ -2,21 +2,42 @@
 
 import pandas as pd
 
+from . import app
+
 
 class Model:
     def __init__(self, conn):
-        self.conn = conn
-        self.basic = '''
-            select * from {} where date_time >= '2017-01-01';
-        '''
+        self.data_directory = f'{app.static_folder}/data'
+        self.ridership = pd.read_csv(
+            f'{self.data_directory}/ridership_since_dec.csv')
+        self.ridership.set_index('date', inplace=True)
+        self.ridership.columns = self.ridership.columns.str.upper()
+        self.ratios = pd.read_csv(
+            f'{self.data_directory}/ratio_ues.csv')
+        self.ratios.set_index('unit', inplace=True)
+        self.details = pd.read_csv(
+            f'{self.data_directory}/line_station_color_details.csv')
+        self.station_data = None
+        self.affected_station_data = None
 
-    def load_data(self, unit, daily=False):
-        data = pd.read_sql(self.basic.format(unit), self.conn,
-                           parse_dates=['date_time'])
-        if daily:
-            data['date'] = data['date_time'].dt.date
-            return data.groupby('date')[['enter', 'exit']].sum().reset_index()
-        return data
+    def update(self, unit):
+        self.station_data = self.get_main_station(unit)
+        affected = self.get_most_affected(unit)
+        affected_riders = [self.get_affected_station(u, d)
+                           for u, d in zip(affected.index, affected.values)]
 
-    def get_most_affected(self, unit):
-        pass
+    def get_main_station(self, unit):
+        return self.ridership.loc[:'2017-04-30', unit]
+
+    def get_affected_station(self, unit, delta):
+        riders = self.ridership[unit]
+        riders.loc['2017-05-01':] *= (1 + delta / riders.mean())
+        return riders
+
+    def get_most_affected(self, unit, number=4):
+        effect = self.ratios[unit]
+        # print(effect)
+        station_riders = self.ridership[unit]
+        mean_riders = station_riders.mean()
+        delta_others = 1 / (effect / mean_riders)
+        return delta_others.sort_values().iloc[:number]
--- switchyard/views.py
+++ switchyard/views.py
@@ -14,20 +14,6 @@ from sqlalchemy import create_engine
 from . import app, model
 
 
-def get_stations_associated_with_lines(conn):
-    query = '''
-        select unit, station, linename from details;
-    '''
-    details = pd.read_sql(query, conn)
-    units = details.unit.unique().tolist()
-    stations = [details.station[details.unit == u].iloc[-1] for u in units]
-    lines = [details.linename[details.unit == u].tolist() for u in units]
-    lines = [''.join(sorted(list(set(''.join(l))))) for l in lines]
-    details = list(zip(units, stations, lines))
-    details.sort(key=lambda x: x[1])
-    return details
-
-
 user = 'mikemoran'
 host = 'localhost'
 dbname = 'fullstations'
@@ -36,9 +22,8 @@ conn = None
 conn = psycopg2.connect(database=dbname, user=user)
 model = model.Model(conn)
 
-details = get_stations_associated_with_lines(conn)
 filename = f'{app.static_folder}/data/line_station_color_details.csv'
-details_df = pd.read_csv(filename).set_index('unit')
+details_df = pd.read_csv(filename)
 
 
 @app.route('/')
@@ -48,22 +33,23 @@ def index():
     index = request.args.get('i')
     unit = 'R001' if unit is None else unit
     index = 1 if index is None else index
-    data, color = pull_data(unit)
+    model.update(unit)
+    details = model.details.values.tolist()
+    details.sort(key=lambda x: x[1])
+    color = pull_data(unit)
     logo_file = get_random_logo()
     return render_template('index.html',
                            unit=unit,
                            index=index,
                            details=details,
                            logo_file=logo_file,
-                           data=data,
+                           data=model.station_data,
                            color=color)
 
 
 def pull_data(unit):
-    data = model.load_data(unit, True)
-    print(data.head())
-    color = details_df.loc[unit, 'color']
-    return data, color
+    color = details_df.color[details_df.unit == unit].values[0]
+    return color
 
 
 def get_random_logo():
